cb59fc76832f45f7a1b7990c66942172e2ed44a4
Yulia Kozlova <rcoldhan@gmail.com>
2021-03-11 14:36:36 +0300

    final

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,6 @@ import java.util.regex.Pattern;
 import static com.company.Country.getByRuName;
 
 public class Main {
-
     public static void main(final String[] args) {
         Scanner console = new Scanner(System.in);
 
@@ -17,7 +16,6 @@ public class Main {
 
         System.out.print("Введи название страны на английском: ");
 
-        //while (true) {
         String countryName = console.nextLine();
         Pattern pattern = Pattern.compile("^[a-zA-Z]+$");
         Matcher matcher = pattern.matcher(countryName);
@@ -25,28 +23,28 @@ public class Main {
             countryName = countryName.toUpperCase();
         }
 
-        Country result;
+        Country userCountry;
         try {
-            result = Country.valueOf(countryName);
-            if (result.getIsOpen()) {
-                System.out.println("Страна " + result.toString() + " открыта для посещения.");
-            } else {
-                System.out.println("Страна " + result.toString() + " закрыта для посещения.");
-            }
+            userCountry = Country.valueOf(countryName);
+            checkIsOpen(userCountry);
         } catch (IllegalArgumentException e) {
             System.out.println("Наименование страны на английском введено некорректно, проверяем русское название...");
             try {
-                result = getByRuName(countryName);
-                if (result.getIsOpen()) {
-                    System.out.println("Страна " + result.toString() + " открыта для посещения.");
-                } else {
-                    System.out.println("Страна " + result.toString() + " закрыта для посещения.");
-                }
+                userCountry = getByRuName(countryName);
+                checkIsOpen(userCountry);
             } catch (NoSuchCountryException ex) {
                 System.out.println("Страны '" + countryName + "' не существует.");
             }
         } catch (NullPointerException e) {
-            System.out.println("Неверный ввод! Попробуй еще раз ввести название страны...");
+            System.out.println("Ошибка при выполнении программы.");
+        }
+    }
+
+    private static void checkIsOpen(final Country country) {
+        if (country.getIsOpen()) {
+            System.out.println("Страна [" + country.toString() + "] открыта для посещения.");
+        } else {
+            System.out.println("Страна [" + country.toString() + "] закрыта для посещения.");
         }
     }
 }
